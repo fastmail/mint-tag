c82dd578454109f08d904947ac4cac4d8997009d
Michael McClimon <michael@mcclimon.org>
2020-02-21 10:23:35 -0500

    Refactor config a bit
    Mostly to remove the dumb validation.

--- lib/Buildotron/Config.pm
+++ lib/Buildotron/Config.pm
@@ -5,96 +5,90 @@ use Moo;
 use experimental 'postderef';
 
 use TOML::Parser;
+use Types::Standard qw(HashRef Str);
 
 # I tried using Module::Runtime for this, but failed mysteriously. TODO: why??
 use Buildotron::Remote::Github;
 use Buildotron::Remote::GitLab;
 
-has _cfg => (
-  is => 'ro',
-  required => 1,
-  init_arg => 'cfg',
-);
-
 sub from_file {
   my ($class, $file) = @_;
 
   my $config = TOML::Parser->new->parse_file($file);
-  $class->_validate_config($config);
 
-  return $class->new({ cfg => $config });
+  return $class->new({
+    cfg                => $config,
+    local_repo_dir     => $config->{local}{path},
+    target_branch_name => $config->{local}{target_branch},
+    upstream_base      => $config->{local}{upstream_base},
+    remotes            => $config->{remote},
+    meta               => $config->{meta} // {},
+  });
 }
 
-# Surely I could do this in some other way...
-sub _assert_exists {
-  my ($class, $cfg, $path) = @_;
+has _cfg => (
+  is => 'ro',
+  required => 1,
+  init_arg => 'cfg',
+  isa => HashRef,
+);
 
-  my @parts = split /[.]/, $path;
-  my @seen;
-
-  my $cur = $cfg;
-
-  while (@parts) {
-    my $k = shift @parts;
-    push @seen, $k;
-
-    $cur = $cur->{$k};
-
-    unless ($cur) {
-      my $full = join q{.}, @seen;
-      die "Missing config item $full!\n" unless $cur;
-    }
-  }
-
-  return;
-}
-
-sub _validate_config {
-  my ($class, $cfg) = @_;
-
-  $class->_assert_exists($cfg, 'local.path');
-  $class->_assert_exists($cfg, 'local.target_branch');
-  $class->_assert_exists($cfg, 'local.upstream_base');
-
-  my $remotes = $cfg->{remote} // {};
-
-  for my $remote (keys %$remotes) {
-    $class->_assert_exists($cfg, "remote.$remote.url");
-    $class->_assert_exists($cfg, "remote.$remote.interface_class");
-  }
-}
+has meta => (
+  is => 'ro',
+  isa => HashRef,
+  required => 1,
+);
 
 has local_repo_dir => (
   is => 'ro',
-  lazy => 1,
-  builder => sub { $_[0]->_cfg->{local}{path} },
+  isa => Str,
+  required => 1,
 );
 
 has target_branch_name => (
   is => 'ro',
-  lazy => 1,
-  builder => sub { $_[0]->_cfg->{local}{target_branch} },
+  isa => Str,
+  required => 1,
 );
 
 has upstream_base => (
   is => 'ro',
-  lazy => 1,
-  builder => sub { $_[0]->_cfg->{local}{upstream_base} },
+  isa => Str,
+  required => 1,
+);
+
+has remotes => (
+  is => 'ro',
+  isa => sub {
+    my $val = shift;
+    die "must be a hashref\n" unless ref $val eq 'HASH';
+
+    for my $name (keys %$val) {
+      my $remote = $val->{$name};
+      die "remote '$name' must be a hashref\n" unless ref $remote eq 'HASH';
+      die "remote '$name' is missing value for 'url'\n" unless $remote->{url};
+      die "remote '$name' is missing value for 'interface_class'\n"
+        unless $remote->{interface_class};
+    }
+
+    return;
+  },
+  required => 1,
 );
 
 # return a list of the remotes, in some order.
 sub remote_names {
   my $self = shift;
 
-  my $remotes = $self->_cfg->{meta}{remote_order};
-  return @$remotes if $remotes;
+  my $ordered = $self->meta->{remote_order};
+  return @$ordered if $ordered;
 
-  return sort keys $self->_cfg->{remote}->%*;
+  return sort keys $self->remotes->%*;
 }
 
 sub remote_interface_for {
   my ($self, $remote_name) = @_;
-  my $this_cfg = $self->_cfg->{remote}{$remote_name};
+  my $this_cfg = $self->remotes->{$remote_name};
 
   die "No configuration for remote named $remote_name!\n"
     unless $this_cfg;
