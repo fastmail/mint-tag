5aeeddad8b20185499cfe689a935921e9f1216c1
Lukas Erlacher <lerlacher@fastmailteam.com>
2024-03-05 13:07:10 +1100

    PLAT-1751: Tag pruning
    This allows mint-tag to prune old tags. We have too many tags and we
    don't want to add to the fire.
    
    Specifically, this introduces a new `cleanup_tag_days` config option
    that can be passed for build steps that have `push_tag_to` set and if
    set to a nonzero number all tags that match the `tag_prefix` and have a
    commit date older than (roughly) that number of days will be deleted
    from the remote.

--- lib/App/MintTag.pm
+++ lib/App/MintTag.pm
@@ -391,6 +391,8 @@ sub maybe_push ($self, $step, $tagname = undef) {
   $self->_maybe_push_branch_for_step($step);
 
   $self->_maybe_delete_source_branches($step);
+
+  $self->_maybe_cleanup_tags($step);
 }
 
 sub _maybe_force_push_rebased_branches ($self, $step) {
@@ -491,6 +493,42 @@ sub _maybe_delete_source_branches ($self, $step) {
       ])
     };
   }
+}
+
+sub _maybe_cleanup_tags ($self, $step) {
+  return unless $step->push_tag_to && $step->cleanup_tag_days;
+
+  my $remote = $step->push_tag_to;
+  my $prefix = $step->tag_prefix;
+
+  try {
+    my $tag_spec = "refs/tags/${prefix}*";
+    my $tag_list = run_git('for-each-ref', "--format=%(refname:short)\t%(creatordate:unix)", $tag_spec);
+
+    my @tags = map {; my ($tag, $ts) = split /\t/; { ts => $ts, 'tag' => $tag} }
+    split /\r?\n/, $tag_list;
+
+    my $time = time;
+
+    my $max_ts = $time - ($step->cleanup_tag_days * 86400);
+
+    for my $chunk (@tags) {
+      if ($chunk->{ts} < $max_ts) {
+        $Logger->log(["Deleting old tag %s", $chunk->{tag}]);
+        run_git('push', $remote->name, ":refs/tags/$chunk->{tag}");
+      } else {
+        $Logger->log(["Keeping old tag %s", $chunk->{tag}]);
+      }
+    }
+  } catch {
+    my $err = $_;
+
+    $Logger->log_fatal([
+        "could not delete remote tag on remote %s: %s",
+        $remote->name,
+        $err,
+      ])
+  };
 }
 
 sub finalize ($self) {
